f418f4501bb04fc74283118ae0b8b1f755239aa0
Mayara Ascari <97797937+AscariMay@users.noreply.github.com>
2023-10-17 21:50:14 -0300

    Update index.js

--- Atividade9/index.js
+++ Atividade9/index.js
@@ -7,9 +7,9 @@ let idadeMaisVelha = 0;
 let idadeMaisNova = Infinity;
 
 for (let i = 0; i < 45; i++) {
-    let idade = parseInt(prompt("Informe a idade da pessoa " + (i + 1) + ":"));
-    let sexo = prompt("Informe o sexo da pessoa ([M] para masculino, [F] para feminino):").toUpperCase();
-    let opiniao = parseInt(prompt("Informe a opinião da pessoa ([1] para péssimo, [2] para regular, [3] para bom, [4] para ótimo):"));
+    const idade = parseInt(prompt(`Informe a idade da pessoa ${i + 1}:`));
+    const sexo = prompt("Informe o sexo da pessoa ([M] para masculino, [F] para feminino):").toUpperCase();
+    const opiniao = parseInt(prompt("Informe a opinião da pessoa ([1] para péssimo, [2] para regular, [3] para bom, [4] para ótimo):"));
 
     totalIdade += idade;
     idadeMaisVelha = Math.max(idadeMaisVelha, idade);
@@ -17,9 +17,7 @@ for (let i = 0; i < 45; i++) {
 
     if (opiniao === 1) {
         countPessimo++;
-    }
-
-    if (opiniao === 4 || opiniao === 3) {
+    } else if (opiniao === 3 || opiniao === 4) {
         countOtimoBom++;
     }
 
@@ -33,10 +31,10 @@ for (let i = 0; i < 45; i++) {
 const mediaIdade = totalIdade / 45;
 const porcentagemOtimoBom = (countOtimoBom / 45) * 100;
 
-console.log("Média de idade: " + mediaIdade);
-console.log("Idade da pessoa mais velha: " + idadeMaisVelha);
-console.log("Idade da pessoa mais nova: " + idadeMaisNova);
-console.log("Quantidade de pessoas que responderam péssimo: " + countPessimo);
-console.log("Porcentagem de pessoas que responderam ótimo e bom: " + porcentagemOtimoBom + "%");
-console.log("Número de mulheres que responderam ao questionário: " + countMulheres);
-console.log("Número de homens que responderam ao questionário: " + countHomens);
+console.log(`Média de idade: ${mediaIdade.toFixed(2)}`);
+console.log(`Idade da pessoa mais velha: ${idadeMaisVelha}`);
+console.log(`Idade da pessoa mais nova: ${idadeMaisNova}`);
+console.log(`Quantidade de pessoas que responderam péssimo: ${countPessimo}`);
+console.log(`Porcentagem de pessoas que responderam ótimo e bom: ${porcentagemOtimoBom.toFixed(2)}%`);
+console.log(`Número de mulheres que responderam ao questionário: ${countMulheres}`);
+console.log(`Número de homens que responderam ao questionário: ${countHomens}`);
