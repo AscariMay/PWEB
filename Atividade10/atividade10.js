ea28f659df3d749c65ef3de4c16bd362ad879a07
Mayara Ascari <97797937+AscariMay@users.noreply.github.com>
2023-10-17 21:43:51 -0300

    Add files via upload

---
+++ Atividade10/atividade10.js
@@ -0,0 +1,12 @@
+function calcularMaiorEOrdenarCrescente(n1, n2, n3) {
+    const arr = [n1, n2, n3];
+    
+    const maior = Math.max(...arr); // Encontra o maior número
+    const ordenado = arr.slice().sort((a, b) => a - b); // Cria uma cópia ordenada da matriz original
+
+    return { maior, ordenado };
+}
+
+const { maior, ordenado } = calcularMaiorEOrdenarCrescente(6, 1, 5);
+console.log(`Maior número: ${maior}`);
+console.log(`Ordenado em ordem crescente: ${ordenado}`);
